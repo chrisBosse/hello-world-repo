5258a62ec58cdbb1c97ebbe0c7355dc66704b857
Chris Bosse <30784811+chrisBosse@users.noreply.github.com>
2017-09-19 02:08:05 +0000

    Comment out some of the install commmands

--- snip/live-machine-setup.sh
+++ snip/live-machine-setup.sh
@@ -3,11 +3,11 @@
 # Install and configure my favorite settings
 # on a freshly booted Linux LIVE ISO
 
-sudo apt install git vim -y
+# sudo apt install git vim -y
 git config --global user.email "30784811+chrisBosse@users.noreply.github.com"
 git config --global user.name "Chris Bosse"
 git config --global core.editor "vim"
-git clone https://github.com/chrisBosse/hello-world-repo.git \
-  && cd hello-world-repo/ \
-  && git status \
-  && git log
+# git clone https://github.com/chrisBosse/hello-world-repo.git \
+#   && cd hello-world-repo/ \
+#   && git status \
+#   && git log
