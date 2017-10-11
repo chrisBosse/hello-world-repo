2628b667c41d7365baedf629717490b0cdfdb26d
Chris Bosse <30784811+chrisBosse@users.noreply.github.com>
2017-10-11 06:56:17 -0400

    Minor git push setting tweak

--- snip/quick-git-setup.sh
+++ snip/quick-git-setup.sh
@@ -5,3 +5,5 @@
 git config --global user.email "30784811+chrisBosse@users.noreply.github.com"
 git config --global user.name "Chris Bosse"
 git config --global core.editor "vim"
+# supresses warning prompt from Git 2.0 with conservative choice
+git config --global push.default simple
