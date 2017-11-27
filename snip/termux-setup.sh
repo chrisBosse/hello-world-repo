4ebb2220af7f2f7cb223e2b7f9c390d3a6a69e9d
Chris Bosse <30784811+chrisBosse@users.noreply.github.com>
2017-11-26 21:43:20 -0500

    Add $HOME/bin to $PATH

--- snip/termux-setup.sh
+++ snip/termux-setup.sh
@@ -7,6 +7,12 @@
 # ./termux-quick-git-setup.sh
 # cd -
 
+echo "# set PATH so it includes user's private bin if it exists" >> $HOME/.bashrc
+echo "if [ -d \"\$HOME/bin\" ] ; then" >> $HOME/.bashrc
+echo "    PATH=\"\$HOME/bin:\$PATH\"" >> $HOME/.bashrc
+echo "fi" >> $HOME/.bashrc
+echo "" >> $HOME/.bashrc
+echo "# set some aliases" >> $HOME/.bashrc
 echo "alias ls='ls -aCF --color=auto'" >> $HOME/.bashrc
 echo "alias ll='ls -l'" >> $HOME/.bashrc
 
